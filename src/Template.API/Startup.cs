009ae660a180e48ffaae3a3e1bc6e66b30ffa7d5
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-04-15 15:28:59 +0200

    Added further configuration to API layer.

--- src/Template.API/Startup.cs
+++ src/Template.API/Startup.cs
@@ -1,36 +1,59 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Template.API.Filters;
+using Template.API.Services;
+using Template.Application;
+using Template.Application.Contrats;
+using Template.Identity;
+using Template.Infrastructure;
+using Template.Persistence;
 
-namespace Template.API
+namespace GloboEvent.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
+            AddSwagger(services);
+            services.AddApplicationService();
+            services.AddValidatorsFromAssembly(typeof(Startup).Assembly);
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddInfrastructureServices(Configuration);
+            services.AddPersistenceService(Configuration);
+            services.AddIdentityServices(Configuration);
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<ILoggedInUserService, LoggedInUserService>();
+
+            services.AddControllers(o => o.Filters.Add(typeof(ResponseMappingFilter)));
+
+            services.AddCors(opt =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Template.API", Version = "v1" });
+                opt.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             });
         }
 
@@ -40,20 +63,71 @@ namespace Template.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Template.API v1"));
             }
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseRouting();
-
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "Globo Event Api");
+                opt.RoutePrefix = "";
+            });
+
+            app.UseCors("Open");
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+        }
+
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc(
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "Template - WebApi",
+                        Description = "This Api will be responsible for overall data distribution and authorization.",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "HeliosCreation",
+                            Email = "reliableDevelopment@hotmail.com",
+                        }
+                    });
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "JWT Authentication",
+                    Description = "Enter JWT Bearer token **_only_**",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer", // must be lower case
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                opt.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {securityScheme, new string[] { }}
+                });
+                opt.OperationFilter<FileResultContentTypeOperationFilter>();
+            });
+
         }
     }
 }
