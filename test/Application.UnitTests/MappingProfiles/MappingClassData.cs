5d7eee9133d1141efbe51c83ffe532246eb0790d
Quentin Couissinier <reliabledevelopment@outlook.com>
2022-04-27 09:26:47 +0200

    Added automapper unit tests.

---
+++ test/Application.UnitTests/MappingProfiles/MappingClassData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Template.Application.Features.Categories;
+using Template.Application.Features.Categories.Commands.Create;
+using Template.Application.Features.Categories.Commands.Update;
+using Template.Application.Features.Categories.Queries.GetCategoryWithEvent;
+using Template.Application.Features.Events.Commands.CreateEvent;
+using Template.Application.Features.Events.Commands.UpdateEvent;
+using Template.Application.Features.Events.Queries.GetEventDetails;
+using Template.Application.Features.Events.Queries.GetEventExport;
+using Template.Application.Features.Events.Queries.GetEventList;
+using Template.Domain.Entities;
+
+namespace Application.UnitTests.MappingProfiles
+{
+    public static class MappingClassData
+    {
+        public class MappingSets : IEnumerable<object[]>
+        {
+            private readonly List<object[]> _data = new List<object[]>
+                {
+                    new object[] {typeof(Event),typeof(EventListVm) },
+                    new object[] {typeof(Event),typeof(EventDetailVm) },
+                    new object[] {typeof(Event),typeof(EventExportDto) },
+                    new object[] {typeof(Event),typeof(EventDto) },
+                    new object[] {typeof(CreateEventCommand),typeof(Event) },
+                    new object[] {typeof(Event),typeof(EventListVm) },
+                    new object[] {typeof(UpdateEventCommand),typeof(Event) },
+
+                    new object[] {typeof(Category),typeof(CategoryVm) },
+                    new object[] {typeof(Category),typeof(CategoryWithEventsVm) },
+                    new object[] {typeof(CreateCategoryCommand),typeof(Category) },
+                    new object[] {typeof(UpdateCategoryCommand),typeof(Category) },
+                };
+
+            public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
---
+++ test/Application.UnitTests/MappingProfiles/MappingTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using System;
+using System.Runtime.Serialization;
+using Template.Application.Profiles;
+using Xunit;
+
+namespace Application.UnitTests.MappingProfiles
+{
+    using static MappingClassData;
+    public class MappingTests
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+
+        [Theory]
+        [ClassData(typeof(MappingSets))]
+        public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = GetInstanceOf(source);
+
+            _mapper.Map(instance, source, destination);
+        }
+
+        private object GetInstanceOf(Type type)
+        {
+            if (type.GetConstructor(Type.EmptyTypes) != null)
+                return Activator.CreateInstance(type);
+
+            // Type without parameterless constructor
+            return FormatterServices.GetUninitializedObject(type);
+        }
+    }
+}
